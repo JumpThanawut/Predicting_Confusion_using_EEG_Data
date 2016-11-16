0043354624cf503676b161a00efb2482c59005a8
Jump Thanawut <thanawutanan@hotmail.com>
2016-11-15 22:04:21 -0800

    Add Logistic Regression.

--- code/Predicting_Confusion_from_EEG.r
+++ code/Predicting_Confusion_from_EEG.r
@@ -8,14 +8,28 @@ cor(subset(eeg_self, select=-c(SelfDefinedConfusion)), eeg_self$SelfDefinedConfu
 
 # Cross-validation preparation
 n = nrow(eeg_self)
+fold = 10
 getCVIndex = function(n, fold) {
   set.seed(1)
-  cv_set = 1:n
+  cv_set = rep(0,n)
   for(i in 1:n) {
-    cv_set[i] = i%%10 + 1
+    cv_set[i] = i%%fold + 1
   }
   cv_set = sample(cv_set, n)
   return(cv_set)
 }
-cv_set = getCVIndex(n, 10)
+cv_set = getCVIndex(n, fold)
 
+# Logistic Regression
+logistic_regression.accuracy = rep(0, fold)
+for (i in 1:fold) {
+  train = eeg_self[cv_set == i,]
+  test = eeg_self[cv_set != i,]
+  logistic_regression.model = glm(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, family = "binomial")
+  summary(logistic_regression.model)
+  logistic_regression.prob = predict(logistic_regression.model, newdata = test, type = "response")
+  logistic_regression.pred = ifelse(logistic_regression.prob > 0.5, 1, 0)
+  logistic_regression.accuracy[i] = mean(logistic_regression.pred == test$SelfDefinedConfusion)
+  #print(paste0("Accuracy(Logistic Regression): ", logistic_regression.accuracy[i]))
+}
+print(paste0(paste0("Accuracy of 10-fold Logistic Regression = ", format(round(mean(logistic_regression.accuracy)*100, 2), nsmall = 2)), "%"))
