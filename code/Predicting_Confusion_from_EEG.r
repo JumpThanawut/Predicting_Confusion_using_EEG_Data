006017878b3545b41985244b312e364fe920a88b
Jump Thanawut <thanawutanan@hotmail.com>
2016-11-15 21:47:49 -0800

    Add splitting data for cross-validation.

--- code/Predicting_Confusion_from_EEG.r
+++ code/Predicting_Confusion_from_EEG.r
@@ -2,3 +2,20 @@
 eeg_raw = read.csv("../data/EEG.csv")
 # Remove pre-defined confusion. We will use self-defined confusion as a target.
 eeg_self = subset(eeg_raw, select=-c(PreDefinedConfusion))
+
+# See correlation of features and target. Max is 0.15. Min is -0.12. No strong correlation of features and target has been found.
+cor(subset(eeg_self, select=-c(SelfDefinedConfusion)), eeg_self$SelfDefinedConfusion)
+
+# Cross-validation preparation
+n = nrow(eeg_self)
+getCVIndex = function(n, fold) {
+  set.seed(1)
+  cv_set = 1:n
+  for(i in 1:n) {
+    cv_set[i] = i%%10 + 1
+  }
+  cv_set = sample(cv_set, n)
+  return(cv_set)
+}
+cv_set = getCVIndex(n, 10)
+
