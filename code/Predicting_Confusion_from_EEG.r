10df1127adbd2df26a8b323a14fe71d026fc3dff
Jump Thanawut <thanawutanan@hotmail.com>
2016-11-19 12:44:49 -0800

    Add Leave one subject-video out cross validation.

--- code/Predicting_Confusion_from_EEG.r
+++ code/Predicting_Confusion_from_EEG.r
@@ -57,21 +57,47 @@ for (i in 1:fold) {
 print(paste0(paste0("Accuracy of 5-fold Logistic Regression without Subject 6 = ", format(round(weighted.mean(logistic_regression.accuracy, n_fold)*100, 2), nsmall = 2)), "%"))
 
 # Visualization
-pdf("../result/plot_raw.pdf", paper = "USr", height = 8, width = 10)
-for (subject in 0:9) {
-  plot(1:nrow(eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == 0,]), eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == 0,]$Raw, main = paste0("Raw Plot of Subject ", subject), xlab = "Time", ylab = "Raw", type="l", col="#00000000")
-  for (i in 1:9) { 
-    if (eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == i,]$SelfDefinedConfusion[1] == 1) {
-      color = "#FF0000FF"
-    }
-    else {
-      color = "#00FF00FF"
-    }
-    lines(1:nrow(eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == i,]), eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == i,]$Raw, type="l", col=color) 
-  } 
+features = c("SubjectID", "VideoID", "Attention", "Meditation", "Raw", "Delta", "Theta", "Alpha1", "Alpha2", "Beta1", "Beta2", "Gamma1", "Gamma2", "PreDefinedConfusion", "SelfDefinedConfusion")
+for (feature in 3:13) {
+  pdf(paste0(paste0("../result/visualization/plot_", features[feature]),".pdf"), paper = "USr", height = 8, width = 10)
+  for (subject in 0:9) {
+    plot(1:nrow(eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == 0,]), eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == 0,features[feature]], main = paste0(paste0(features[feature]," of Subject"), subject), xlab = "Time", ylab = features[feature], type="l", col="#00000000", lwd = 0.5)
+    for (i in 0:9) { 
+      if (eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == i,]$SelfDefinedConfusion[1] == 1) {
+        color = "#FF0000FF"
+      }
+      else {
+        color = "#00CC00FF"
+      }
+      lines(1:nrow(eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == i,]), eeg_raw[eeg_raw$SubjectID == subject & eeg_raw$VideoID == i,features[feature]], type="l", col=color, lwd = 0.5) 
+    } 
+  }
+  dev.off()
 }
-dev.off()
 
+# Leave one subject-video out cross validation.
+n_example = 0
+n_correct_example = 0
+uniqueSubjectID = unique(eeg_self$SubjectID)
+for (subjectID in uniqueSubjectID) {
+  uniqueVideoID = unique(eeg_self[eeg_self$SubjectID == subjectID,]$VideoID)
+  for (videoID in uniqueVideoID) {
+    n_example = n_example + 1
+    train = eeg_self[(eeg_self$SubjectID != subjectID) | (eeg_self$VideoID != videoID),]
+    test = eeg_self[(eeg_self$SubjectID == subjectID) & (eeg_self$VideoID == videoID),]
+    logistic_regression.model = glm(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, family = "binomial")
+    logistic_regression.prob = predict(logistic_regression.model, newdata = test, type = "response")
+    logistic_regression.pred = ifelse(logistic_regression.prob > 0.5, 1, 0)
+    real_class = test[1,]$SelfDefinedConfusion
+    predict_class = ifelse(mean(logistic_regression.pred) > 0.5, 1, 0)
+    if (real_class == predict_class) {
+      n_correct_example = n_correct_example + 1
+      print(paste0(subjectID, videoID))
+    }
+  }
+}
+accuracy = n_correct_example * 100 / n_example
+print(paste0("Accuracy(Leave One Subject-Video Out Logistic Regression): ", accuracy))
 
 
 
