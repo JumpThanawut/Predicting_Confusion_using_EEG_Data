f1910019b7e05544b3c634d224dae9e7932e4f0c
Min Chen <chenmin.artemis@gmail.com>
2016-11-27 23:13:50 -0800

    add accuracies of leave-one-out-cross validation under 6 situations: norm and/or aggregated and/or delta

--- code/Predicting_Confusion_from_EEG.r
+++ code/Predicting_Confusion_from_EEG.r
@@ -3,6 +3,15 @@ eeg_raw = read.csv("../data/EEG.csv")
 # Remove pre-defined confusion. We will use self-defined confusion as a target.
 eeg_self = subset(eeg_raw, select=-c(PreDefinedConfusion))
 
+# Get normalized data
+library("e1071")
+library(caret)
+library(dplyr)
+library(class)
+set.seed(123)
+preObj = preProcess(eeg_self[,-c(1,2,12)], method=c("BoxCox"))
+norm_eeg = predict(preObj, eeg_self)
+
 # See correlation of features and target. Max is 0.15. Min is -0.12. No strong correlation of features and target has been found.
 cor(subset(eeg_self, select=-c(SelfDefinedConfusion)), eeg_self$SelfDefinedConfusion)
 
@@ -22,6 +31,10 @@ for(eeg_rawVector in listBySubjectID){
 # Remove Group index
 eeg_aggregated = aggreData[,-c(1)]
 eeg_aggregated = eeg_aggregated[!eeg_aggregated$SelfDefinedConfusion != 0 || 1]
+
+# Get normalized aggregated data
+preObj = preProcess(eeg_aggregated[,-c(12)], method=c("BoxCox"))
+normAggre_eeg = predict(preObj, eeg_aggregated)
 
 # Cross-validation preparation
 n = nrow(eeg_self)
@@ -93,163 +106,168 @@ for (feature in 3:13) {
 }
 
 # Leave one subject-video out cross validation.
-n_example = 0
-n_correct_example = 0
-n_correct_example_lda = 0
-n_correct_example_qda = 0
-n_correct_example_knn = 0
-n_correct_example_tree = 0
-n_correct_example_rf = 0
-n_correct_example_svm = 0
-n_correct_example_nn = 0
-n_correct_example_bst = 0
+losocv = function(eeg_self) {
+  n_example = 0
+  n_correct_example = 0
+  n_correct_example_lda = 0
+  n_correct_example_qda = 0
+  n_correct_example_knn = 0
+  n_correct_example_tree = 0
+  n_correct_example_rf = 0
+  n_correct_example_svm = 0
+  n_correct_example_nn = 0
+  n_correct_example_bst = 0
+  
+  uniqueSubjectID = unique(eeg_self$SubjectID)
+  for (subjectID in uniqueSubjectID) {
+    uniqueVideoID = unique(eeg_self[eeg_self$SubjectID == subjectID,]$VideoID)
+    for (videoID in uniqueVideoID) {
+      n_example = n_example + 1
+      train = eeg_self[(eeg_self$SubjectID != subjectID) | (eeg_self$VideoID != videoID),]
+      test = eeg_self[(eeg_self$SubjectID == subjectID) & (eeg_self$VideoID == videoID),]
+      # train_aggre = eeg_aggregated[(eeg_aggregated$SubjectID != subjectID) | (eeg_aggregated$VideoID != videoID),]
+      # test_aggre = eeg_aggregated[(eeg_aggregated$SubjectID == subjectID) & (eeg_aggregated$VideoID == videoID),]
+      real_class = test[1,]$SelfDefinedConfusion
+      # real_aggreClass = test_aggre[1,]$SelfDefinedConfusion
+      
+      # logistic regression
+      logistic_regression.model = glm(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, family = "binomial")
+      logistic_regression.prob = predict(logistic_regression.model, newdata = test, type = "response")
+      logistic_regression.pred = ifelse(logistic_regression.prob > 0.5, 1, 0)
+      predict_class = ifelse(mean(logistic_regression.pred) > 0.5, 1, 0)
+      if (real_class == predict_class) {
+        n_correct_example = n_correct_example + 1
+        print(paste0(subjectID, videoID))
+      }
+      
+      # linear discriminant analysis
+      require(MASS)
+      linear_discriminant_analysis.model = lda(SelfDefinedConfusion~.-SubjectID-VideoID, data = train)
+      linear_discriminant_analysis.pred = predict(linear_discriminant_analysis.model, newdata = test)
+      predict_class_lda = linear_discriminant_analysis.pred$class
+      linear_discriminant_analysis.pred$class
+      if (real_class == predict_class_lda) {
+        n_correct_example_lda = n_correct_example_lda + 1
+        print(paste0(subjectID, videoID))
+      }
+  
+      # quadratic discriminant analysis
+      quadratic_discriminant_analysis.model = qda(SelfDefinedConfusion~.-SubjectID-VideoID, data = train)
+      quadratic_discriminant_analysis.pred = predict(quadratic_discriminant_analysis.model, newdata = test)
+      predict_class_qda = quadratic_discriminant_analysis.pred$class
+      if (real_class == predict_class_qda) {
+        n_correct_example_qda = n_correct_example_qda + 1
+        print(paste0(subjectID, videoID))
+      }
+  
+      # k nearest neighbor
+      require(class)
+      # Have tried k=1,2,3,4,5,6,10,13,20,23,50,100. k=1 gets the highest accuracy.
+      knn.pred = knn(train, test, train$SelfDefinedConfusion, k = 1)
+      predict_class_knn = knn.pred
+      if (real_class == predict_class_knn) {
+        n_correct_example_knn = n_correct_example_knn + 1
+        print(paste0(subjectID, videoID))
+      }
+  
+      # # decision tree
+      # require(ISLR)
+      # require(tree)
+      # decision_tree.model = tree(as.factor(SelfDefinedConfusion)~.-SubjectID-VideoID, data = as.factor(train))
+      # decision_tree.pred = predict(decision_tree.model, test, type = "class")
+      # cv.decision_tree = cv.tree(decision_tree.model,FUN = prune.misclass)
+      # prune.decision_tree = prune.misclass(decision_tree.model, best = 10)
+      # decision_tree.pred = predict(prune.decision_tree, test, type = "class")
+      # 
+      # predict_class_tree = decision_tree.pred
+      # if (real_class == predict_class_tree) {
+      #   n_correct_example_tree = n_correct_example_tree + 1
+      #   print(paste0(subjectID, videoID))
+      # }
+      # 
+      # # random forest
+      # require(randomForest)
+      # rf.model = randomForest(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, mtry=5, ntree=100, cutoff = 2, importance=TRUE)
+      # rf.probs = predict(rf.model,newdata = test)
+      # rf.pred = rep(0, length(rf.probs))
+      # rf.pred[rf.probs>=0.5] = 1
+      # predict_class_rf = rf.pred
+      # if (real_class == predict_class_rf) {
+      #   n_correct_example_rf = n_correct_example_rf + 1
+      #   print(paste0(subjectID, videoID))
+      # }
 
-uniqueSubjectID = unique(eeg_self$SubjectID)
-for (subjectID in uniqueSubjectID) {
-  uniqueVideoID = unique(eeg_self[eeg_self$SubjectID == subjectID,]$VideoID)
-  for (videoID in uniqueVideoID) {
-    n_example = n_example + 1
-    train = eeg_self[(eeg_self$SubjectID != subjectID) | (eeg_self$VideoID != videoID),]
-    test = eeg_self[(eeg_self$SubjectID == subjectID) & (eeg_self$VideoID == videoID),]
-    train_aggre = eeg_aggregated[(eeg_aggregated$SubjectID != subjectID) | (eeg_aggregated$VideoID != videoID),]
-    test_aggre = eeg_aggregated[(eeg_aggregated$SubjectID == subjectID) & (eeg_aggregated$VideoID == videoID),]
-    real_class = test[1,]$SelfDefinedConfusion
-    real_aggreClass = test_aggre[1,]$SelfDefinedConfusion
-    
-    # logistic regression
-    logistic_regression.model = glm(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, family = "binomial")
-    logistic_regression.prob = predict(logistic_regression.model, newdata = test, type = "response")
-    logistic_regression.pred = ifelse(logistic_regression.prob > 0.5, 1, 0)
-    predict_class = ifelse(mean(logistic_regression.pred) > 0.5, 1, 0)
-    if (real_class == predict_class) {
-      n_correct_example = n_correct_example + 1
-      print(paste0(subjectID, videoID))
-    }
-    
-    # linear discriminant analysis
-    require(MASS)
-    linear_discriminant_analysis.model = lda(SelfDefinedConfusion~.-SubjectID-VideoID, data = train)
-    linear_discriminant_analysis.pred = predict(linear_discriminant_analysis.model, newdata = test)
-    predict_class_lda = linear_discriminant_analysis.pred$class
-    if (real_class == predict_class_lda) {
-      n_correct_example_lda = n_correct_example_lda + 1
-      print(paste0(subjectID, videoID))
-    }
-
-    # quadratic discriminant analysis
-    quadratic_discriminant_analysis.model = qda(SelfDefinedConfusion~.-SubjectID-VideoID, data = train)
-    quadratic_discriminant_analysis.pred = predict(quadratic_discriminant_analysis.model, newdata = test)
-    predict_class_qda = quadratic_discriminant_analysis.pred$class
-    if (real_class == predict_class_qda) {
-      n_correct_example_qda = n_correct_example_qda + 1
-      print(paste0(subjectID, videoID))
-    }
-
-    # k nearest neighbor
-    require(class)
-    # Have tried k=1,2,3,4,5,6,10,13,20,23,50,100. k=1 gets the highest accuracy.
-    knn.pred = knn(train, test, train$SelfDefinedConfusion, k = 1)
-    predict_class_knn = knn.pred
-    if (real_class == predict_class_knn) {
-      n_correct_example_knn = n_correct_example_knn + 1
-      print(paste0(subjectID, videoID))
-    }
-
-    # decision tree
-    require(ISLR)
-    require(tree)
-    decision_tree.model = tree(as.factor(SelfDefinedConfusion)~.-SubjectID-VideoID, data = train)
-    decision_tree.pred = predict(decision_tree.model, test, type = "class")
-    cv.decision_tree = cv.tree(decision_tree.model,FUN = prune.misclass)
-    prune.decision_tree = prune.misclass(decision_tree.model, best = 10)
-    decision_tree.pred = predict(prune.decision_tree, test, type = "class")
-
-    predict_class_tree = decision_tree.pred
-    if (real_class == predict_class_tree) {
-      n_correct_example_tree = n_correct_example_tree + 1
-      print(paste0(subjectID, videoID))
-    }
-
-    # random forest
-    require(randomForest)
-    rf.model = randomForest(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, mtry=5, ntree=100, cutoff = 2, importance=TRUE)
-    rf.probs = predict(rf.model,newdata = test)
-    rf.pred = rep(0, length(rf.probs))
-    rf.pred[rf.probs>=0.5] = 1
-    predict_class_rf = rf.pred
-    if (real_class == predict_class_rf) {
-      n_correct_example_rf = n_correct_example_rf + 1
-      print(paste0(subjectID, videoID))
-    }
-
-    # support vector machine
-    require("e1071")
-    svm.model = svm(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, kernal = "poly")
-    svm.probs = predict(svm.model, test)
-    svm.pred = rep(0, length(svm.probs))
-    svm.pred[svm.probs>=0.5] = 1
-    # svm.pred = predict(svm.model, test)
-    predict_class_svm = svm.pred
-    if (real_class == predict_class_svm) {
-      n_correct_example_svm = n_correct_example_svm + 1
-      print(paste0(subjectID, videoID))
-    }
-
-    # neural network
-    require("nnet")
-    ideal_train = class.ind(train$SelfDefinedConfusion)
-    neural_network.model = nnet(train[-1-2-14], ideal_train, size=10, softmax=TRUE)
-    neural_network.pred = predict(neural_network.model, test, type = "class")
-    prediction_class_nn = neural_network.pred
-
-    if (real_class == prediction_class_nn) {
-      n_correct_example_nn = n_correct_example_nn + 1
-      print(paste0(subjectID, videoID))
-    }
-    
-    # boosting
-    require(gbm)
-    bst.model = gbm(SelfDefinedConfusion~.-SubjectID-VideoID,data=train,distribution="gaussian",n.trees=6000,shrinkage=0.01,interaction.depth=4)
-    n.trees=seq(from=100,to=10000,by=100)
-    predmat=predict(bst.model,newdata=test,n.trees=6000)
-    boosting.pred = rep(0, length(predmat))
-    boosting.pred[predmat>0.5] = 1
-    prediction_class_nn = boosting.pred
-    if (real_class == prediction_class_nn) {
-      n_correct_example_bst = n_correct_example_bst + 1
-      print(paste0(subjectID, videoID))
+      # # support vector machine
+      # require("e1071")
+      # svm.model = svm(SelfDefinedConfusion~.-SubjectID-VideoID, data = train, kernal = "poly")
+      # svm.probs = predict(svm.model, test)
+      # svm.pred = rep(0, length(svm.probs))
+      # svm.pred[svm.probs>=0.5] = 1
+      # # svm.pred = predict(svm.model, test)
+      # predict_class_svm = svm.pred
+      # if (real_class == predict_class_svm) {
+      #   n_correct_example_svm = n_correct_example_svm + 1
+      #   print(paste0(subjectID, videoID))
+      # }
+      # 
+      # # neural network
+      # require("nnet")
+      # ideal_train = class.ind(train$SelfDefinedConfusion)
+      # neural_network.model = nnet(train[-1-2-14], ideal_train, size=10, softmax=TRUE)
+      # neural_network.pred = predict(neural_network.model, test, type = "class")
+      # prediction_class_nn = neural_network.pred
+      # 
+      # if (real_class == prediction_class_nn) {
+      #   n_correct_example_nn = n_correct_example_nn + 1
+      #   print(paste0(subjectID, videoID))
+      # }
+      # 
+      # # boosting
+      # require(gbm)
+      # bst.model = gbm(SelfDefinedConfusion~.-SubjectID-VideoID,data=train,distribution="gaussian",n.trees=6000,shrinkage=0.01,interaction.depth=4)
+      # n.trees=seq(from=100,to=10000,by=100)
+      # predmat=predict(bst.model,newdata=test,n.trees=6000)
+      # boosting.pred = rep(0, length(predmat))
+      # boosting.pred[predmat>0.5] = 1
+      # prediction_class_nn = boosting.pred
+      # if (real_class == prediction_class_nn) {
+      #   n_correct_example_bst = n_correct_example_bst + 1
+      #   print(paste0(subjectID, videoID))
+      # }
     }
   }
+  
+  accuracy = n_correct_example * 100 / n_example
+  accuracy_lda = n_correct_example_lda * 100 / n_example
+  accuracy_qda = n_correct_example_qda * 100 / n_example
+  accuracy_knn = n_correct_example_knn * 100 / n_example
+  accuracy_tree = n_correct_example_tree * 100 / n_example
+  accuracy_rf = n_correct_example_rf * 100 / n_example
+  accuracy_svm = n_correct_example_svm * 100 / n_example
+  accuracy_nn = n_correct_example_nn * 100 / n_example
+  accuracy_bst = n_correct_example_bst * 100 / n_example
+  
+  print(paste0("Accuracy(Leave One Subject-Video Out Logistic Regression): ", accuracy))
+  # 63
+  print(paste0("Accuracy(Leave One Subject-Video Out Linear Discriminant Analysis Regression): ", accuracy_lda))
+  # 52
+  print(paste0("Accuracy(Leave One Subject-Video Out Quadratic Discriminant Analysis Regression): ", accuracy_qda))
+  # 50
+  print(paste0("Accuracy(Leave One Subject-Video Out K Nearest Neighbor): ", accuracy_knn))
+  # 61
+  print(paste0("Accuracy(Leave One Subject-Video Out Decision Tree): ", accuracy_tree))
+  # 56
+  print(paste0("Accuracy(Leave One Subject-Video Out Random Forest): ", accuracy_rf))
+  # 53
+  print(paste0("Accuracy(Leave One Subject-Video Out Support Vector Machine): ", accuracy_svm))
+  # 53
+  print(paste0("Accuracy(Leave One Subject-Video Out Neural Network): ", accuracy_nn))
+  # 55
+  print(paste0("Accuracy(Leave One Subject-Video Out Neural Network): ", accuracy_bst))
+  # 
+  result = list("accuracy"=accuracy,"accuracy_lda"=accuracy_lda,"accuracy_qda"=accuracy_qda,"accuracy_knn"=accuracy_knn,"accuracy_tree"=accuracy_tree,"accuracy_rf"=accuracy_rf,"accuracy_svm"=accuracy_svm,"accuracy_nn"=accuracy_nn)
+  return(result)
 }
-
-accuracy = n_correct_example * 100 / n_example
-accuracy_lda = n_correct_example_lda * 100 / n_example
-accuracy_qda = n_correct_example_qda * 100 / n_example
-accuracy_knn = n_correct_example_knn * 100 / n_example
-accuracy_tree = n_correct_example_tree * 100 / n_example
-accuracy_rf = n_correct_example_rf * 100 / n_example
-accuracy_svm = n_correct_example_svm * 100 / n_example
-accuracy_nn = n_correct_example_nn * 100 / n_example
-accuracy_bst = n_correct_example_bst * 100 / n_example
-
-print(paste0("Accuracy(Leave One Subject-Video Out Logistic Regression): ", accuracy))
-# 63
-print(paste0("Accuracy(Leave One Subject-Video Out Linear Discriminant Analysis Regression): ", accuracy_lda))
-# 52
-print(paste0("Accuracy(Leave One Subject-Video Out Quadratic Discriminant Analysis Regression): ", accuracy_qda))
-# 50
-print(paste0("Accuracy(Leave One Subject-Video Out K Nearest Neighbor): ", accuracy_knn))
-# 61
-print(paste0("Accuracy(Leave One Subject-Video Out Decision Tree): ", accuracy_tree))
-# 56
-print(paste0("Accuracy(Leave One Subject-Video Out Random Forest): ", accuracy_rf))
-# 53
-print(paste0("Accuracy(Leave One Subject-Video Out Support Vector Machine): ", accuracy_svm))
-# 53
-print(paste0("Accuracy(Leave One Subject-Video Out Neural Network): ", accuracy_nn))
-# 55
-print(paste0("Accuracy(Leave One Subject-Video Out Neural Network): ", accuracy_bst))
-# 
 
 # Cross-validation preparation with aggregated data
 aggre_eeg_self = subset(eeg_aggregated, select=-c(PreDefinedConfusion))
@@ -400,6 +418,15 @@ accuracy.svm.normAggre = svm.fit(normAggre_eeg, cv_set)
 accuracy.rf.normAggre = rf.fit(normAggre_eeg, cv_set)
 accuracy.bst.normAggre = bst.fit(normAggre_eeg, cv_set)
 
+# Result accuracy list of Leave-one-out-cross validation on original eeg data: 63,52,50,61
+losocv.original = losocv(eeg_self)
+# Result accuracy list of Leave-one-out-cross validation on normalized eeg data: 62,49,44,64
+losocv.norm = losocv(norm_eeg)
+# Result accuracy list of Leave-one-out-cross validation on aggregated eeg data: 63,55,55,61
+losocv.aggregated = losocv(eeg_aggregated)
+# Result accuracy list of Leave-one-out-cross validation on normalized aggregated eeg data: 61,57,49,55
+losocv.aggregated.norm = losocv(normAggre_eeg)
+
 # Add more features related to delta
 eeg_aggregated$d_attention = NA
 eeg_aggregated$d_meditation = NA
@@ -463,6 +490,11 @@ accuracy.svm.d_aggreNor = svm.fit(normAggre_d_eeg, cv_set)
 accuracy.rf.d_aggreNor = rf.fit(normAggre_d_eeg, cv_set)
 accuracy.bst.d_aggreNor = bst.fit(normAggre_d_eeg, cv_set)
 
+# Result accuracy list of Leave-one-out-cross validation on Delta version of aggregated eeg data:68,53,47,43
+losocv.delta.aggregated = losocv(eeg_aggregated)
+# Result accuracy list of Leave-one-out-cross validation on Delta version of normalized aggregated eeg data: 63,54,53,52
+losocv.delta.aggregated.norm = losocv(normAggre_d_eeg)
+
 # plot accuracy vs different processed data
 accuracy.table = matrix(c(accuracy, accuracy_svm, accuracy_knn, accuracy_rf, accuracy_bst,
                           accuracy.lm, accuracy.svm, accuracy.knn, accuracy.RF, accuracy.Boosting,
@@ -484,7 +516,6 @@ ggplot(accuracy.long,aes(x=Var1,y=value,fill=Var2))+
   geom_bar(stat="identity",position="dodge")+
   xlab("Different data procession")+ylab("Mean Accuracy")+
   ggtitle("Accuracy Comparison")
-
 
 
 
